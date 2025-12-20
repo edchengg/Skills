b49b3955ecf3b3bf265a62a14e275bf95205c8a1
Yang Chen <yachen@cw-pdx-cs-001-login-02.cm.cluster>
2025-12-19 17:22:35 -0800

    add config

---
+++ scripts/synthesize_mathproof_dpsk_v32.py
@@ -0,0 +1,33 @@
+from nemo_skills.pipeline.cli import generate, wrap_arguments, eval
+
+cluster = "slurm"
+output_dir = "/lustre/fsw/portfolios/llmservice/users/yachen/AceMath/Skills/deepseek-v32-sdg"
+gpus=8
+server_nodes=2
+i=0
+generate(
+    ctx=wrap_arguments(
+        "++skip_filled=True "
+        "++prompt_config=lean4/formal-proof "
+        "++inference.top_p=0.95 "
+        "++inference.tokens_to_generate=120000 " 
+        "++max_concurrent_requests=1024 "
+        "++inference.endpoint_type=chat "
+        "++chat_template_kwargs.thinking=true "
+    ),
+    cluster=cluster,
+    model="deepseek-ai/DeepSeek-V3.2-Speciale",
+    with_sandbox=False,
+    expname=f"generate_{i}",
+    server_type='sglang',
+    server_gpus=gpus,
+    partition='batch',
+    server_nodes=server_nodes,
+    num_chunks=1,
+    dependent_jobs=0,
+    starting_seed=0,
+    num_random_seeds=1,
+    input_file="/lustre/fsw/portfolios/llmservice/projects/llmservice_fm_text/users/yachen/AceMath/AceProof/nemotron_math_proofs_v1_aops.jsonl",
+    output_dir=f"{output_dir}/nemotron_math_proofs_v1_aops/",
+    server_args=f"--ep-size {gpus * server_nodes} --dp {gpus * server_nodes} --enable-dp-attention --reasoning-parser deepseek-v3 --log-requests --mem-fraction-static=0.8",
+)
