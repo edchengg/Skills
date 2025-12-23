3ba78857709f9170320922f1fc1b6a3257be1ca1
Yang Chen <yachen@cw-pdx-cs-001-login-02.cm.cluster>
2025-12-23 10:50:06 -0800

    imo_answerbench

---
+++ scripts/eval_imo_answerbench_gptoss.py
@@ -0,0 +1,50 @@
+from nemo_skills.pipeline.cli import generate, wrap_arguments, eval
+
+cluster = "slurm"  # change this to match your cluster config name
+model_name = "gpt-oss-120b"
+output_dir = "/lustre/fsw/portfolios/llmservice/users/yachen/AceMath/Skills/gpt-oss-120b-eval/"
+benchmark = "imo_answerbench"
+
+eval(
+    ctx=wrap_arguments(
+        # we are using fewer tokens than max context length as code output isn't accounted for
+        "++inference.tokens_to_generate=96000 "
+        # recommended inference settings including prompt config
+        "++inference.temperature=1.0 "
+        "++inference.top_p=1.0 "
+        "++prompt_config=gpt-oss/math "
+        # we currently implement native Python code tool through text completions API
+        # as we found alternative implementations to have issues.
+        # We will switch to the official responses API when the support is added
+        "++inference.endpoint_type=text "
+        "++code_tags=gpt-oss "
+        # gpt-oss generates a lot of code, so need to set max_code_executions high!
+        # you can also add ++server.code_execution.code_execution_timeout=120 to match
+        # the setting in the official system prompt, but we found this to not impact
+        # the accuracy, so keeping the default of 10 seconds
+        "++code_execution=true "
+        "++server.code_execution.max_code_executions=100 "
+        # settings to enable high reasoning and Python built-in tool
+        "++chat_template_kwargs.reasoning_effort=high "
+        "++chat_template_kwargs.builtin_tools=[python] "
+    ),
+    cluster=cluster,
+    # optional parameter here, but useful when chaining multiple jobs together in pipelines
+    expname="gpt-oss-eval-math-with-python",
+    # can customize the number of GPUs used
+    server_gpus=8,
+    # any vllm arguments can be used here
+    server_args="--async-scheduling",
+    # launch a sandbox alongside the job that will keep track of
+    # ipython sessions with stateful code execution
+    with_sandbox=True,
+    dependent_jobs=0,
+    cluster=cluster,
+    expname=f"{model_name}-with-python",
+    model="openai/gpt-oss-120b",
+    server_type='vllm',
+    server_gpus=8,
+    num_chunks=1,
+    benchmarks=f"{benchmark}:1",
+    output_dir=output_dir + model_name + "/with-python",
+)
