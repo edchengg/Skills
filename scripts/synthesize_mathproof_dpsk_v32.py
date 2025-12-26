66d1b4e0f66bbf5731ab9669c8546cf8e67a7d1b
Yang Chen <yachen@cw-pdx-cs-001-login-02.cm.cluster>
2025-12-25 18:05:08 -0800

    x

--- scripts/synthesize_mathproof_dpsk_v32.py
+++ scripts/synthesize_mathproof_dpsk_v32.py
@@ -10,7 +10,7 @@ generate(
         "++skip_filled=True "
         "++prompt_config=gpt-oss/math_proof_gen "
         "++inference.top_p=0.95 "
-        "++inference.tokens_to_generate=96000 " 
+        "++inference.tokens_to_generate=65536 " 
         "++max_concurrent_requests=1024 "
         "++inference.endpoint_type=chat "
         "++chat_template_kwargs.thinking=true "
@@ -23,10 +23,10 @@ generate(
     server_gpus=gpus,
     partition='batch',
     server_nodes=server_nodes,
-    num_chunks=2,
-    dependent_jobs=24,
+    num_chunks=16,
+    dependent_jobs=6,
     starting_seed=0,
-    num_random_seeds=8,
+    num_random_seeds=1,
     input_file="/lustre/fsw/portfolios/llmservice/projects/llmservice_fm_text/users/yachen/AceMath/AceProof/nemotron_math_proofs_v1_aops_min10000.jsonl",
     output_dir=f"{output_dir}/nemotron_math_proofs_v1_aops_min10000/",
     server_args=f"--ep-size {gpus * server_nodes} --dp {gpus * server_nodes} --enable-dp-attention --reasoning-parser deepseek-v3 --log-requests --mem-fraction-static=0.8",
