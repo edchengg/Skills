6779945b915cceaf6478c2bf761e3b2eee28ea64
Yang Chen <yachen@nvidia.com>
2025-12-31 07:26:42 +0000

    x

--- scripts/eval_math_dpsk_v32.py
+++ scripts/eval_math_dpsk_v32.py
@@ -27,5 +27,5 @@ eval(
     dependent_jobs=0,
     benchmarks=f"aime25:1",
     output_dir=f"{output_dir}/",
-    server_args=f"--ep-size {gpus * server_nodes} --dp {gpus * server_nodes} --enable-dp-attention --reasoning-parser deepseek-v3 --log-requests --mem-fraction-static=0.8 --model-loader-extra-config {{\\\"enable_multithread_load\\\":true,\\\"num_threads\\\":32}}",
+    server_args=f"--ep-size {gpus * server_nodes} --dp {gpus * server_nodes} --enable-dp-attention --reasoning-parser deepseek-v3 --log-requests --mem-fraction-static=0.8",
 )
