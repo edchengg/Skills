f53cd67596ed89bd09b6f1873addaf6fa1fc9ea5
Yang Chen <yachen@cw-pdx-cs-001-login-02.cm.cluster>
2025-12-31 11:02:21 -0800

    x

--- scripts/passrate_mathrl_zihan_sft.py
+++ scripts/passrate_mathrl_zihan_sft.py
@@ -9,51 +9,51 @@ model_name = "zihan-sft-10992"
 model_path = "/lustre/fsw/portfolios/llmservice/users/zihanl/inform/megatron2hf/llm_ft/Post-Training/megatron-lm/checkpoints/sft_gptoss_v1_1_32nodes_allpurpose_5e-5_32_262144/safetensors-checkpoint-10992"
 
 
-generate(
-    ctx=wrap_arguments(
-        "++inference.tokens_to_generate=120000 "
-        "++inference.temperature=1.0 "
-        "++inference.top_p=1.0 "
-        "++prompt_config=generic/math_sft_notool "
-    ),
-    cluster=cluster,
-    expname=f"{model_name}-no-python",
-    model=model_path,
-    server_type='vllm',
-    num_chunks=1,
-    server_args="--mamba_ssm_cache_dtype float32 --no-enable-prefix-caching",
-    output_dir=output_dir + model_name + "/curriculum_v7_acrid-teal_main_rename.filtered",
-    # can customize the number of GPUs used
-    server_gpus=8,
-    input_file="/lustre/fsw/portfolios/llmservice/projects/llmservice_fm_text/users/yachen/AceMath/AceProof/data_processing_rl/curriculum_v7_acrid-teal_main_rename.filtered.jsonl",
-    dependent_jobs=4,
-    starting_seed=0,
-    num_random_seeds=16,
-)
-
 # generate(
 #     ctx=wrap_arguments(
-#         # we are using fewer tokens than max context length as code output isn't accounted for
-#         "++inference.tokens_to_generate=96000 "
-#         # recommended inference settings including prompt config
+#         "++inference.tokens_to_generate=120000 "
 #         "++inference.temperature=1.0 "
 #         "++inference.top_p=1.0 "
-#         "++prompt_config=judge/imo-answerbench "
-#         "++inference.endpoint_type=text "
-#         "++code_tags=gpt-oss "
-#         "++chat_template_kwargs.reasoning_effort=high "
+#         "++prompt_config=generic/math_sft_notool "
 #     ),
-#     generation_type="math_judge",
-#     model="openai/gpt-oss-120b",
 #     cluster=cluster,
-#     expname=f"{model_name}-judge",
+#     expname=f"{model_name}-no-python",
+#     model=model_path,
 #     server_type='vllm',
 #     num_chunks=1,
+#     server_args="--mamba_ssm_cache_dtype float32 --no-enable-prefix-caching",
+#     output_dir=output_dir + model_name + "/curriculum_v7_acrid-teal_main_rename.filtered",
 #     # can customize the number of GPUs used
 #     server_gpus=8,
-#     input_dir=output_dir + model_name + "/acereasonv2",
-#     output_dir=output_dir + model_name + "/acereasonv2-judge",
-#     dependent_jobs=1,
+#     input_file="/lustre/fsw/portfolios/llmservice/projects/llmservice_fm_text/users/yachen/AceMath/AceProof/data_processing_rl/curriculum_v7_acrid-teal_main_rename.filtered.jsonl",
+#     dependent_jobs=4,
 #     starting_seed=0,
 #     num_random_seeds=16,
 # )
+
+generate(
+    ctx=wrap_arguments(
+        # we are using fewer tokens than max context length as code output isn't accounted for
+        "++inference.tokens_to_generate=96000 "
+        # recommended inference settings including prompt config
+        "++inference.temperature=1.0 "
+        "++inference.top_p=1.0 "
+        "++prompt_config=judge/imo-answerbench "
+        "++inference.endpoint_type=text "
+        "++code_tags=gpt-oss "
+        "++chat_template_kwargs.reasoning_effort=high "
+    ),
+    generation_type="math_judge",
+    model="openai/gpt-oss-120b",
+    cluster=cluster,
+    expname=f"{model_name}-judge",
+    server_type='vllm',
+    num_chunks=1,
+    # can customize the number of GPUs used
+    server_gpus=8,
+    input_dir=output_dir + model_name + "/acereasonv2",
+    output_dir=output_dir + model_name + "/acereasonv2-judge",
+    dependent_jobs=0,
+    starting_seed=0,
+    num_random_seeds=16,
+)
