6ffed77acc4198a36f8ff3d4dccbc2b16add2148
Yang Chen <yachen@cw-pdx-cs-001-login-02.cm.cluster>
2025-12-31 12:24:05 -0800

    gpqa eval sft

--- scripts/eval_gpqa_nano_v3_sft_full.py
+++ scripts/eval_gpqa_nano_v3_sft_full.py
@@ -1,12 +1,12 @@
 from nemo_skills.pipeline.cli import generate, wrap_arguments, eval
 
 cluster = "slurm"
-output_dir = "/lustre/fsw/portfolios/llmservice/users/yachen/AceMath/Skills/nano-v3-eval/"
+output_dir = "/lustre/fsw/portfolios/llmservice/users/yachen/AceMath/Skills/nano-v3-sft-eval"
 gpus=8
 server_nodes=1
 i=0
-model_name = "nano-v3"
-model_path = "/lustre/fsw/portfolios/llmservice/projects/llmservice_fm_text/users/yachen/AceMath/checkpoint/nvidia-nemotron-3-nano-30b-a3b-bf16"
+model_name = "nano-v3-sft"
+model_path = "/lustre/fsw/portfolios/llmservice/projects/llmservice_fm_text/users/yachen/AceMath/checkpoint/nvidia-nemotron-3-nano-30b-a3b-sft"
 for benchmark in ["gpqa"]:
     eval(
         ctx=wrap_arguments(
@@ -40,7 +40,7 @@ for benchmark in ["gpqa"]:
     #     server_gpus=8,
     #     num_chunks=1,
     #     with_sandbox=True,
-    #     benchmarks=f"{benchmark}:8",
+    #     benchmarks=f"{benchmark}:64",
     #     server_args="--mamba_ssm_cache_dtype float32 --no-enable-prefix-caching --enable-auto-tool-choice --tool-call-parser qwen3_coder",
     #     output_dir=output_dir + model_name + "/with-python",
     # )
