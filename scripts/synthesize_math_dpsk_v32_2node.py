74857b0f83e4506dbf8aca34afb37bc8131049a9
Yang Chen <yachen@nvidia.com>
2026-01-03 21:00:26 +0000

    debug 2 node script

---
+++ scripts/synthesize_math_dpsk_v32_2node.py
@@ -0,0 +1,34 @@
+from nemo_skills.pipeline.cli import generate, wrap_arguments, eval
+
+cluster = "slurm"
+output_dir = "/scratch/fsw/portfolios/llmservice/users/yachen/AceMath/Skills/deepseek-v32-sdg"
+gpus=8
+server_nodes=2
+i=0
+generate(
+    ctx=wrap_arguments(
+        "++skip_filled=True "
+        "++prompt_config=qwen/math-cot "
+        "++inference.top_p=0.95 "
+        "++inference.tokens_to_generate=120000 " 
+        "++max_concurrent_requests=1024 "
+        "++inference.endpoint_type=chat "
+        "++chat_template_kwargs.thinking=true "
+    ),
+    cluster=cluster,
+    model="/scratch/fsw/portfolios/llmservice/users/yachen/cache/DeepSeek-V3.2-Speciale",
+    server_container="/scratch/fsw/portfolios/llmservice/users/yachen/AceMath/container/nemo-skills-sglang-v32.sqsh",
+    with_sandbox=False,
+    expname=f"generate_{i}",
+    server_type='sglang',
+    server_gpus=gpus,
+    partition='batch',
+    server_nodes=server_nodes,
+    num_chunks=1,
+    dependent_jobs=0,
+    starting_seed=0,
+    num_random_seeds=1,
+    input_file="/scratch/fsw/portfolios/llmservice/users/yachen/AceMath/AceProof/aimo_aops_raw_nonproof_n9_filtered.min10000.jsonl",
+    output_dir=f"{output_dir}/aimo_aops_raw_nonproof_n9_filtered.min10000/",
+    server_args=f"--ep-size {gpus * server_nodes} --dp {gpus * server_nodes} --enable-dp-attention --reasoning-parser deepseek-v3 --log-requests --mem-fraction-static=0.8",
+)
